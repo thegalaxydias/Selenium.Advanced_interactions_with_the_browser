f30f2794dbb7110162d5e423bb9e3b38ce6f5880
Demchenko Kirill <thegalaxydias@gmail.com>
2021-10-11 21:41:53 +0300

    Добавьте файлы проекта.

---
+++ Selenium.Advanced_interactions_with_the_browser/Program.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+
+namespace Selenium.Advanced_interactions_with_the_browser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://uitestpractice.com/");
+
+            IWebElement drag = driver.FindElement(By.Id("draggable"));
+            IWebElement drop = driver.FindElement(By.Id("droppable"));
+
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            IWebElement element = wait.Until(ExpectedConditions.ElementToBeClickable(drag));
+           
+            Actions builder = new Actions(driver);
+
+            builder.DragAndDrop(element, drop).Build().Perform();
+
+            
+        }
+    }
+}
